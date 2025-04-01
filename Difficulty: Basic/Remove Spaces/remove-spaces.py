043bf875876ade0709794973aa8e55f895474b6c
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-04-01 20:35:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def modify(self, s):
+        # code here
+        a=s.replace(' ','')
+        return a
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        ob = Solution()
+        ans = ob.modify(s)
+        print(ans)
+        print("~")
+# } Driver Code Ends
