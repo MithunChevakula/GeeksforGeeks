9f030496b030e8470e3cae4b5f580d18c8b12518
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-04-22 21:13:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findUnique(self, arr):
+        # code here 
+        res=0
+        for i in arr:
+            res^=i
+        return res
+            
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.findUnique(arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
