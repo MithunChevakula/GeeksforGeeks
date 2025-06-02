40a503c61897d8e62be552da4e4864d330ab58a0
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-02 10:00:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique Paths in a Grid/unique-paths-in-a-grid.py
@@ -0,0 +1,48 @@
+class Solution:
+    def uniquePaths(self, grid):
+        # code here 
+        n = len(grid)
+
+        m = len(grid[0])
+
+
+
+        if grid[0][0] == 1 or grid[n-1][m-1] == 1:
+
+            return 0
+
+        
+
+        # Initialize a 2D dp array with 0s
+
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+
+        
+
+        # Starting point
+
+        dp[0][0] = 1
+
+        
+
+        for i in range(n):
+
+            for j in range(m):
+
+                if grid[i][j] == 1:
+
+                    dp[i][j] = 0 # Cannot pass through blocked cells
+
+                else:
+
+                    if i > 0:
+
+                        dp[i][j] += dp[i-1][j]
+
+                    if j > 0:
+
+                        dp[i][j] += dp[i][j-1]
+
+        
+
+        return dp[n-1][m-1]
