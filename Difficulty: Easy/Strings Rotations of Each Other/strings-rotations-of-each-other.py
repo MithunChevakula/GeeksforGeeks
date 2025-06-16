d16211cf80831223b7c9a3188acf2fb66cfa2437
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-16 20:18:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        #code here
+        if len(s1)==len(s2) and s2 in s1+s1:
+            return True
+        return False
