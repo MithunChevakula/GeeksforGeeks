de7c7235380ade205f347f15724e020435ad9130
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-16 17:30:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.py
@@ -0,0 +1,18 @@
+'''
+class Node:
+    def _init_(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+# your task is to complete this function
+
+class Solution:
+    #Function to convert a binary tree into its mirror tree.
+    def mirror(self, root):
+        # Code here
+        if not root:
+            return
+        root.left,root.right = root.right,root.left
+        self.mirror(root.left)
+        self.mirror(root.right)
