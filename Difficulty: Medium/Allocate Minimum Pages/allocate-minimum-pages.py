699ada55c00d593330dedb329a387dd481d46376
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-17 21:38:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -0,0 +1,40 @@
+class Solution:
+    
+    #Function to find minimum number of pages.
+    def findPages(self, arr, k):
+        #code here
+        if len(arr) < k:
+            return -1
+        
+        # Helper function to check feasibility of allocation
+        def isPossible(arr, n, k, maxPages):
+            student_count = 1
+            current_pages = 0
+            
+            for pages in arr:
+                if current_pages + pages > maxPages:
+                    # Allocate books to next student
+                    student_count += 1
+                    current_pages = pages
+                    
+                    # If students required exceed k, return False
+                    if student_count > k:
+                        return False
+                else:
+                    current_pages += pages
+            
+            return True
+        
+        # Binary search over the range of possible max pages
+        low, high = max(arr), sum(arr)
+        result = high
+        
+        while low <= high:
+            mid = (low + high) // 2
+            if isPossible(arr, len(arr), k, mid):
+                result = mid  # Update result and try for a smaller max
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return result
