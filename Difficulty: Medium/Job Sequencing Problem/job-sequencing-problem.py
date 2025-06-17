7279caab348bf5934a2f07987daff9c07ee7b5db
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-17 13:38:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jobSequencing(self, deadline, profit):
+        # code here
+        import heapq
+        pairs = sorted(zip(deadline, profit), key=lambda x: (x[0], -x[1]))
+        h, cur = [], 0
+        for d, p in pairs:
+            if d > cur:
+                cur += 1
+                heapq.heappush(h, p)
+            elif d == cur:
+                heapq.heappushpop(h, p)
+        return len(h), sum(h)
