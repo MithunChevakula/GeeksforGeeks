7186f2924996f278334a275f9497fd9ad0768f2b
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-06-21 21:59:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.py
@@ -0,0 +1,8 @@
+import statistics
+class Solution:
+    def median(self, mat):
+        new = []
+        for row in matrix:
+            for element in row:
+               new.append(element)
+        return statistics.median(new)    
