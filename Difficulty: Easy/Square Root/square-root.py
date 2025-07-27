29a7e0642a6170dd1d540875ef1a81fd3ff98b48
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-07-27 21:21:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.py
+++ Difficulty: Easy/Square Root/square-root.py
@@ -2,5 +2,5 @@ import math
 class Solution:
     def floorSqrt(self, n): 
         # code here
-        return math.floor(math.sqrt(n))
-        
+        #return math.floor(math.sqrt(n))
+        return int(n**0.5)
