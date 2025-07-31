4644be6f5f3adb5569e362378c7995d8103ace9e
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-07-31 09:16:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Powerful Integer/powerful-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def powerfulInteger(self, intervals, k):
+        # code here
+        from heapq import heappush, heappop
+        
+        intervals.sort()
+        q, ans = [], -1
+        for start, end in intervals:
+            while q and q[0] < start:
+                heappop(q)
+            heappush(q, end)
+            while len(q) >= k:
+                ans = max(ans, heappop(q))
+    
+        while len(q) >= k:
+            ans = max(ans, q[0])
+            heappop(q)
+        
+        return ans
