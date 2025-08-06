bb7e6a1e4d9d7fcaaac176b8604625c59daf89a4
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-06 20:40:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.py
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.py
@@ -1,16 +1,21 @@
-#User function Template for python3
-
 class Solution:
     def romanToDecimal(self, s): 
-        # code here
-        rom={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        res=0
-        i=0
-        while i<len(S):
-            if i+1<len(S) and rom[S[i]]<rom[S[i+1]]:
-                res+=rom[S[i+1]]-rom[S[i]]
-                i+=2
+        key = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        
+        i = 0
+        res = 0
+        while i<len(s):
+            if i == len(s)-1 or key[s[i]] >= key[s[i+1]]:
+                res+=key[s[i]]
             else:
-                 res+=rom[S[i]]
-                 i+=1
+                res-=key[s[i]]
+            i+=1
         return res
