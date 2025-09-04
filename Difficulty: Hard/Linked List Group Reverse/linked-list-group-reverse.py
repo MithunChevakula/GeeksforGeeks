585bb0fb2c5f055406037ce3c3d605fa462b5382
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-09-04 20:55:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.py
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.py
@@ -1,27 +1,27 @@
-"""Node is defined as
-
-class Node:
-    def __init__(self, data):
-		self.data = data
-		self.next = None
-"""
 class Solution:
     def reverseKGroup(self, head, k):
-        # Code here
-        arr = []
-        _head = head
-        _init_head = head
-        counter = 0 
-        while _head:
-            arr.append(_head.data)
-            counter += 1
-            _head = _head.next
-            if counter == k:
-                while arr:
-                    _init_head.data = arr.pop()
-                    _init_head = _init_head.next
-                counter = 0
-        while arr:
-            _init_head.data = arr.pop()
-            _init_head = _init_head.next
-        return head
+        def reverse(sta,sto):
+            prv=None
+            cur=sta
+            while cur!=sto:
+                nxt=cur.next
+                cur.next=prv
+                prv=cur
+                cur=nxt
+            return prv
+        dummy=Node(-1)
+        dummy.next=head
+        tail=dummy
+        sta=head
+        sto=sta
+        while sto:
+            for _ in range(k):
+                sto=sto.next
+                if not sto:
+                    break
+            ntail=reverse(sta,sto)
+            tail.next=ntail
+            tail=sta
+            sta.next=sto
+            sta=sto
+        return dummy.next
