48b868ea0b1a0425b3a274e33f185f7ae3a95a59
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-09-30 09:19:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def binstr(self, n):
+        # code here
+        res= []
+        for i in range(2**n):
+            
+            res.append(format(i, f'0{n}b'))
+        return res
+            
+        
