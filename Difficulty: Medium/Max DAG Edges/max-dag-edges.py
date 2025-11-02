e7020dab1005c9d8c841cac13d9409caa9dad24b
CHEVAKULA MITHUN <155737363+MithunChevakula@users.noreply.github.com>
2025-11-02 11:18:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max DAG Edges/max-dag-edges.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxEdgesToAdd(self, V, edges):
+        # code here
+        E = len(edges)
+        max_total_edges = (V * (V - 1)) // 2
+        additional_edges = max_total_edges - E
+        return additional_edges
